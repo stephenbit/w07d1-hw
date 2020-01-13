c7ad7b8ff2ae9d1667ce4ebb106b6e037783255e
Stephen Ramsay <rabrandom@gmail.com>
2020-01-13 16:52:48 +0000

    done some js stuff

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
 import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', function() {
+new Vue({
+el: "#app",
+data: {
+  items: [
+    {name: "Gather berries". inCompleted: false}
+  ],
+  newItem: ""
+},
+methods: {
+  saveNewItem: function() {
+    this.items.push({
+      name: this.newItem,
+      isPurchased: false
+    });
+    this.newItem = "";
+  };
+  addItem: function(index){
+    this.items[index].isPurchased = true;
+  }
+}
+});
+});
